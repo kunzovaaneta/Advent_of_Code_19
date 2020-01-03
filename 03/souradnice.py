d933cbb3a60c02e9f57402c19c07540b0dc5397a
kunzovaaneta <kunzovaaneta@seznam.cz>
2020-01-03 16:51:15 +0100

    Uprava a optimalizace první části

--- 03/souradnice.py
+++ 03/souradnice.py
@@ -1,18 +1,45 @@
-pohyby = ('R8','U5','L5','D3')
+#pohyby_A = ['R8','U5','L5','D3']
+#pohyby_B = ['U7','R6','D4','L4']
 
-souradnice = [0,0]
-#R/L = right/left - plus/minus one on first position
-#U/D = up/down - plus/minus one on second position
+with open('pohyby_A.txt', encoding='utf-8') as a:
+	pohyby_A = [x.strip().split(',') for x in a][0]
+with open('pohyby_B.txt', encoding='utf-8') as b:
+	pohyby_B = [x.strip().split(',') for x in b][0]
 
-for pohyb in pohyby:
-	if pohyb[0] == 'R':
-		souradnice[0] = souradnice[0] + int(pohyb[1:])
-	#	print(souradnice)
-	elif pohyb[0] == 'L':
-		souradnice[0] = souradnice[0] - int(pohyb[1:])
-	elif pohyb[0] == 'U':
-		souradnice[1] = souradnice[1] + int(pohyb[1:])
-	#	print(souradnice)
-	elif pohyb[0] == 'D':
-		souradnice[1] = souradnice[1] - int(pohyb[1:])
-print(souradnice)
+posun_x = {
+	'U' : 0, 
+	'D' : 0,
+	'R' : 1,
+	'L' : -1
+}
+
+posun_y = {
+	'U' : 1, 
+	'D' : -1,
+	'R' : 0,
+	'L' : 0
+}
+
+def seznam_pohybu(A):
+	x = 0
+	y = 0
+	seznam_pohybu = set()
+	for a in A:
+		smer = a[0]
+		assert smer in ['U','D','R','L'] # kontrola
+		posun = int(a[1:])
+		for _ in range(posun):
+			x += posun_x[smer]
+			y += posun_y[smer]
+			seznam_pohybu.add((x,y))
+	return seznam_pohybu
+
+
+seznam_pohybu_A = seznam_pohybu(pohyby_A)
+seznam_pohybu_B = seznam_pohybu(pohyby_B)
+
+prunik= seznam_pohybu_A&seznam_pohybu_B
+#print(prunik)
+
+min_vzdalenost = min([abs(x) + abs(y) for (x,y) in prunik])
+print(min_vzdalenost)
