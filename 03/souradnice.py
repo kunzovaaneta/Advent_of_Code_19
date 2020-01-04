f9c71cc11bce5875d5eabeb22c6c54cd2d4bf06a
kunzovaaneta <kunzovaaneta@seznam.cz>
2020-01-04 18:07:21 +0100

    Dokončená i druhá část dne.

--- 03/souradnice.py
+++ 03/souradnice.py
@@ -6,24 +6,27 @@ with open('pohyby_A.txt', encoding='utf-8') as a:
 with open('pohyby_B.txt', encoding='utf-8') as b:
 	pohyby_B = [x.strip().split(',') for x in b][0]
 
-posun_x = {
-	'U' : 0, 
-	'D' : 0,
-	'R' : 1,
-	'L' : -1
-}
-
-posun_y = {
-	'U' : 1, 
-	'D' : -1,
-	'R' : 0,
-	'L' : 0
-}
 
 def seznam_pohybu(A):
+	posun_x = {
+		'U' : 0, 
+		'D' : 0,
+		'R' : 1,
+		'L' : -1
+		}
+
+	posun_y = {
+		'U' : 1, 
+		'D' : -1,
+		'R' : 0,
+		'L' : 0
+		}
+
 	x = 0
 	y = 0
-	seznam_pohybu = set()
+	delka = 0
+	seznam_pohybu = dict()
+
 	for a in A:
 		smer = a[0]
 		assert smer in ['U','D','R','L'] # kontrola
@@ -31,15 +34,19 @@ def seznam_pohybu(A):
 		for _ in range(posun):
 			x += posun_x[smer]
 			y += posun_y[smer]
-			seznam_pohybu.add((x,y))
+			delka += 1
+			seznam_pohybu[(x,y)] = delka
 	return seznam_pohybu
-
-
+	
 seznam_pohybu_A = seznam_pohybu(pohyby_A)
 seznam_pohybu_B = seznam_pohybu(pohyby_B)
 
-prunik = seznam_pohybu_A&seznam_pohybu_B
+prunik = set(seznam_pohybu_A.keys())&set(seznam_pohybu_B.keys())
+#print(set(seznam_pohybu_A.keys()))
 #print(prunik)
 
 min_vzdalenost = min([abs(x) + abs(y) for (x,y) in prunik])
-print(min_vzdalenost)
+print('Výsledek první části je {}.'.format(min_vzdalenost))
+
+min_delka_pruniku = min([seznam_pohybu_A[x] + seznam_pohybu_B[x] for x in prunik])
+print('Výsledek druhé části je {}.'.format(min_delka_pruniku))
